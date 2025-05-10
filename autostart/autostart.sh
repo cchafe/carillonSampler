c999edc20dffad63fab4f1d10fe6b91c76ffdf8f
chris chafe <cc@ccrma.stanford.edu>
2025-05-10 15:30:14 -0700

    autostart

--- autostart/autostart.sh
+++ autostart/autostart.sh
@@ -2,5 +2,7 @@ killall jackd
 sleep 1
 /usr/bin/jackd -dalsa -dhw:A96 -r48000 -p1024 &
 sleep 2
-chuck -busize:32 /home/cc/carillonSampler/ck/carillonSamplerMIDI.ck
+chuck -busize:32 /home/cc/carillonSampler/ck/carillonSamplerMIDI.ck &
+sleep 2
+aconnect 28:0 129:0
 
