e8892ed1ed5c7d33423a98825345c841fe676fa4
chris chafe <cc@ccrma.stanford.edu>
2025-06-14 10:16:56 -0700

    fresh rpi5 with chuck-1.5.5.1 updates, no need for jackd

--- autostart/autostart.sh
+++ autostart/autostart.sh
@@ -1,10 +1,11 @@
 # killall jackd
 # killall chuck
-sleep 1
-/usr/bin/jackd -dalsa -dhw:A96 -r48000 -p1024 &
+# sleep 1
+# /usr/bin/jackd -dalsa -dhw:A96 -r48000 -p1024 &
 sleep 2
 cd /home/cc/carillonSampler/ck
 chuck -busize:32 carillonSamplerMIDI.ck &
 sleep 2
-aconnect 28:0 RtMidi\ Input\ Client:0
+# aconnect 28:0 RtMidi\ Input\ Client:0
+aconnect AudioBox\ USB\ 96:0 RtMidi\ Input\ Client:0
 
